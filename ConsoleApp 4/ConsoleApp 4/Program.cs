def9fd08b46561de45229e4d3f7a0ff9a120e99d
DashaMalukova <145812669+DashaMalukova@users.noreply.github.com>
2023-09-23 16:36:56 +0300

    Add files via upload

---
+++ ConsoleApp 4/ConsoleApp 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c = "135";
+            string k = "45";
+
+            Console.WriteLine(c);
+            Console.WriteLine(k);
+        }
+    }
+}
